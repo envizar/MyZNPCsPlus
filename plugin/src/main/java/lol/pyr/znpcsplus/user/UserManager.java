9304c9672332598610ee9b7545e79178fe085cc1
Pyrbu <pyrmcserver@gmail.com>
2024-09-05 14:42:34 +0200

    switch user map to ConcurrentHashMap to prevent errors on Folia

--- plugin/src/main/java/lol/pyr/znpcsplus/user/UserManager.java
+++ plugin/src/main/java/lol/pyr/znpcsplus/user/UserManager.java
@@ -3,12 +3,12 @@ package lol.pyr.znpcsplus.user;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class UserManager {
-    private final Map<UUID, User> userMap = new HashMap<>();
+    private final Map<UUID, User> userMap = new ConcurrentHashMap<>();
 
     public UserManager() {
         Bukkit.getOnlinePlayers().forEach(this::get);
